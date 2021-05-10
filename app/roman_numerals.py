da4e56e90712421bd171f03242f6b9750a760665
Kevin Zheng <jiezheng2020@gmail.com>
2021-05-10 17:20:26 -0500

    refactored phase 8

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -7,6 +7,14 @@ def parse(arg):
     # if arg == 'VI': return 6
     # if arg == 'VII': return 7
     # if arg == 'VIII': return 8
-    count = 0
+    total = 0
+    dict = {"I":1, "V":5, "X": 10, "L":50, "C": 100, "D":500, "M":1000}
 
-    for i in range(len(arg)):
+    for index in range(len(arg) -1):
+        if dict[arg[index]] >= dict[arg[index +1]]:
+            total += dict[arg[index]]
+        else:
+            total -= dict[arg[index]]
+
+    total += dict[arg[-1]]
+    return total
